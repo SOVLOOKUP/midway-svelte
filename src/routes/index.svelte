c67e79574203afa316b973498228906c868954ef
sovlookup <gonorth@qq.com>
2021-05-25 16:03:54 +0800

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+import api from '../apis/lambda'
+import { onMount } from 'svelte'
+
+let message = 'work in progress'
+  onMount(async () => {
+    const response = await api()
+    message = response.message
+  })
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<h2>{message}</h2>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
