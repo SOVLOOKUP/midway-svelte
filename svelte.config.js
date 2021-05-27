38f253deae91436033415a4283b534fce91e0d0c
sovlookup <gonorth@qq.com>
2021-05-27 08:45:51 +0800

    fix: dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: defineConfig({
-			plugins: [ hooks() ]
+			plugins: [ hooks.default() ]
 		})
 	}
 };
